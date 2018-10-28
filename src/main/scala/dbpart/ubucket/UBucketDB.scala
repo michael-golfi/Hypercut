39ece28163ed0d801c4ae4f9e625aa84028486d5
Johan Nystrom <johan@monomorphic.org>
2018-10-28 17:13:46 +0900

    Set-based deduplicating UBucket implementation

--- src/main/scala/dbpart/ubucket/UBucketDB.scala
+++ src/main/scala/dbpart/ubucket/UBucketDB.scala
@@ -6,7 +6,7 @@ import kyotocabinet._
  * Database of unique buckets for k-mers.
  * k-mers with identical sequence data are stored together.
  */
-class UBucketDB(location: String, options: String, separator: String = "\n") {
+class BucketDB(location: String, options: String, separator: String = "\n") {
   
   def addHook() {
     Runtime.getRuntime.addShutdownHook(new Thread {
@@ -26,6 +26,22 @@ class UBucketDB(location: String, options: String, separator: String = "\n") {
   addHook()
   
   
+  /**
+   * The default bucket implementation simply adds to a list.
+   * Overriding methods can be more sophisticated, e.g. perform sorting or deduplication
+   */
+  def setInsertSingle(oldSet: String, value: String): String = 
+    s"$oldSet$separator$value"  
+  
+  def setInsertBulk(oldSet: String, values: Iterable[String]): String = {
+    val newVals = values.mkString(separator)
+    setInsertSingle(oldSet, newVals)
+  }
+  
+  def newSet(values: Iterable[String]) =
+    values.mkString(separator)
+  
+  
   def addSingle(key: String, record: String) {    
     val v = Option(db.get(key))
     v match {
@@ -33,7 +49,7 @@ class UBucketDB(location: String, options: String, separator: String = "\n") {
         if (value.size > 2000) {
           println(s"Key $key size ${value.size}")
         }
-        db.set(key, s"$value$separator$record")
+        db.set(key, setInsertSingle(value, record))
       case None => 
         db.set(key, record)
     }
@@ -44,27 +60,45 @@ class UBucketDB(location: String, options: String, separator: String = "\n") {
   import scala.collection.{Map => CMap}
   import scala.collection.mutable.{Map => MMap}
   
-  def merge(into: MMap[String, String], d2: CMap[String, String]) = {
-    for ((k, v) <- d2) {
+  def merge(into: MMap[String, String], from: CMap[String, Iterable[String]]) = {
+    for ((k, vs) <- from) {
       into.get(k) match {
-        case Some(v1) =>
-          into += k -> s"$v1$separator$v"
+        case Some(existingSet) =>
+          into += k -> setInsertBulk(existingSet, vs)
         case None =>
-          into += k -> v
+          into += k -> newSet(vs)
       }
     }
     into
   }
   
   def addBulk(data: Iterable[(String, String)]) {
-    val insert = data.groupBy(_._1).
-      mapValues(vs => vs.map(_._2).mkString(separator))
+    val insert = data.groupBy(_._1).mapValues(vs => vs.map(_._2))
     
     val keys = insert.keys
     val existing = db.get_bulk(keys.toList.asJava, false)
     val merged = merge(existing.asScala, insert)
     db.set_bulk(merged.asJava, false)
   }
+  
+}
+
+/**
+ * BucketDB implementation that deduplicates entries.
+ */
+class UBucketDB(location: String, options: String, separator: String = "\n") extends BucketDB(location, options, separator) {
+
+  override def setInsertSingle(oldSet: String, value: String): String = 
+    setInsertBulk(oldSet, Seq(value))  
+  
+  override def setInsertBulk(oldSet: String, values: Iterable[String]): String = {
+    val old = Set() ++ oldSet.split(separator, -1)
+    val newSet = old ++ values
+    newSet.mkString(separator)    
+  }
+  
+  override def newSet(values: Iterable[String]) =    
+    values.toSeq.distinct.mkString(separator)
   
 }
 
