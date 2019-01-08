938c78eb64d69ec3fc7cc4d8579d80ac67ebf383
Johan Nystrom <johan@monomorphic.org>
2019-01-08 14:48:56 +0900

    Stats timer

--- src/main/scala/dbpart/SeqPrintBuckets.scala
+++ src/main/scala/dbpart/SeqPrintBuckets.scala
@@ -74,19 +74,28 @@ object SeqPrintBuckets {
         new SeqPrintBuckets(space, k, numMarkers, dbfile).handle(
           FlatQ.stream(Console.in.lines().iterator))
       case "graph" =>
+        Stats.begin()
         val kms = new KmerSpace()
         val k = args(1).toInt //e.g. 31
         val numMarkers = args(2).toInt //e.g. 4
         val dbfile = args(3)
         val buckets = new SeqPrintBuckets(space, k, numMarkers, dbfile)
-        val graph = makeGraph(kms, buckets)                
+        val graph = makeGraph(kms, buckets)   
+        Stats.end("Construct graph")
+        
+        Stats.begin()
         val mg = new MacroGraph(graph)
         val parts = mg.partition(1000)
+        Stats.end("Partition graph")
+        
         val hist = new Histogram(parts.map(_.size), 20)
         hist.print("Macro partition size")
         val numKmers = parts.map(p => buckets.db.getBulk(p.map(_.packedString)).map(_._2.size).sum)
-        new Histogram(numKmers, 20).print("Macro partition expanded size")                
+        new Histogram(numKmers, 20).print("Macro partition expanded size")    
+        
+        Stats.begin()
         val colGraph = CollapsedGraph.construct(parts, graph)
+        Stats.end("Collapse graph")
         
         GraphViz.writeUndirected[CollapsedGraph.G[MarkerSet]](colGraph, "out.dot", 
             ms => ms.nodes.size + ":" + ms.nodes.head.packedString)
