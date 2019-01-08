4974b650c0985ad6d9f46a7967f55d4a26a7cb45
Johan Nystrom <johan@monomorphic.org>
2019-01-08 15:56:09 +0900

    Improve efficiency of FastAdjListGraph by using mutable ArrayBuffer

--- src/main/scala/dbpart/FastAdjListGraph.scala
+++ src/main/scala/dbpart/FastAdjListGraph.scala
@@ -3,22 +3,32 @@ package dbpart
 import scala.collection.mutable.Map
 
 import friedrich.graph.SetBackedGraph
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * A graph that stores nodes in sets and edges in vectors.
  * It is assumed that each edge is added only once, so no
  * deduplication is performed.
  * The underlying implementation is based on mutable data structures for fast construction.
+ * 
+ * Currently this graph only stores forward edges.
+ * It is trivial to change implAddEdge to also store reverse edges.
  */
 class FastAdjListGraph[N <: AnyRef] extends SetBackedGraph[N] {
-  private val adjList = Map[N, Vector[N]]()
-  private val revAdjList = Map[N, Vector[N]]()
+  //If the type of EdgeList is changed, addForward and addBackward may also need to be
+  //updated accordingly for efficiency. e.g. prepend is inefficient for ArrayBuffer
+  type EdgeList = ArrayBuffer[N]
+  
+  private val adjList = Map[N, EdgeList]()
+  private val revAdjList = Map[N, EdgeList]()
 
   override def numEdges = nodes.foldLeft(0)(_ + edgesFrom(_).size)
 
-  def edgesFrom(from: N): Seq[N] = adjList.getOrElse(from, Seq())
+  def emptyEdgeList: EdgeList = ArrayBuffer()
+  
+  def edgesFrom(from: N): Seq[N] = adjList.getOrElse(from, emptyEdgeList)
 
-  def edgesTo(from: N): Seq[N] = revAdjList.getOrElse(from, Seq())
+  def edgesTo(from: N): Seq[N] = revAdjList.getOrElse(from, emptyEdgeList)
   
   def edges: Iterator[(N, N)] = nodes.flatMap(n => {
     edgesFrom(n).map(x => (n, x)) ++ edgesTo(n).map(x => (x, n))
@@ -26,18 +36,26 @@ class FastAdjListGraph[N <: AnyRef] extends SetBackedGraph[N] {
 
   override def implAddEdge(from: N, to: N) {
     addForward(from, to)
-    addBackward(to, from)
+//    addBackward(to, from)
   }
 
   protected def addForward(from: N, to: N) {
-    var ol = adjList.getOrElse(from, Vector())
-    ol :+= to
-    adjList += (from -> ol)
+    val ol = adjList.get(from) match {
+      case Some(list) => list
+      case None => 
+        adjList += (from -> emptyEdgeList)
+        adjList(from)
+    }    
+    ol += to
   }
   
   protected def addBackward(from: N, to: N) {
-    var ol = revAdjList.getOrElse(from, Vector())
-    ol :+= to
-    revAdjList += (from -> ol)
+     val ol = revAdjList.get(from) match {
+      case Some(list) => list
+      case None => 
+        revAdjList += (from -> emptyEdgeList)
+        revAdjList(from)
+    }        
+    ol += to
   }
 }
