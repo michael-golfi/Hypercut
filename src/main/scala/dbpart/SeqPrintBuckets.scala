f9b94e53b12befffc62826e61084f51905e953e6
Johan Nystrom <johan@monomorphic.org>
2019-01-17 16:42:56 +0900

    Print bucket size histogram after build, in terms of sequences and k-mers

--- src/main/scala/dbpart/SeqPrintBuckets.scala
+++ src/main/scala/dbpart/SeqPrintBuckets.scala
@@ -13,9 +13,9 @@ import scala.concurrent.duration.Duration
 class SeqPrintBuckets(val space: MarkerSpace, val k: Int, val numMarkers: Int, dbfile: String) {
   val extractor = new MarkerSetExtractor(space, numMarkers, k)
   val db = new PathBucketDB(dbfile, UBucketDB.options, k)
-  
+
   import scala.concurrent.ExecutionContext.Implicits.global
-  
+
   def precompIterator[A](it: Iterator[A]) = new Iterator[A] {
     def nextBuffer = if (it.hasNext) Some(Future(it.next)) else None
     var buffer = nextBuffer
@@ -32,10 +32,10 @@ class SeqPrintBuckets(val space: MarkerSpace, val k: Int, val numMarkers: Int, d
       }
     }
   }
-  
+
   def handle(reads: Iterator[(String, String)]) {
-    val handledReads = 
-      reads.grouped(100000).map(group => 
+    val handledReads =
+      reads.grouped(100000).map(group =>
       { group.par.flatMap(r => handle(r._1))
       })
 
@@ -43,10 +43,10 @@ class SeqPrintBuckets(val space: MarkerSpace, val k: Int, val numMarkers: Int, d
       db.addBulk(rs.seq)
     }
   }
-  
+
   //TODO: also handle the reverse complement of each read
   //TODO: mate pairs
-  
+
   def handle(read: String) = {
     val kmers = read.sliding(k)
     val mss = extractor.markerSetsInRead(read)
@@ -55,14 +55,14 @@ class SeqPrintBuckets(val space: MarkerSpace, val k: Int, val numMarkers: Int, d
         print(".")
       }
     }
-    
+
     mss.map(_.packedString).iterator zip kmers
   }
 }
 
 object SeqPrintBuckets {
   val space = MarkerSpace.default
-  
+
   def makeGraph(kms: KmerSpace, sbs: SeqPrintBuckets) = {
     val graph = new FastAdjListGraph[MarkerSet]
     for ((key, vs) <- sbs.db.buckets) {
@@ -82,26 +82,33 @@ object SeqPrintBuckets {
       graph.uncheckedAddEdge(from, to)
     }
     println(graph.numEdges + " edges")
-    
+
     Distribution.printStats("Node degree", graph.nodes.map(graph.degree))
-    
+
     val hist = new Histogram(Seq() ++ graph.nodes.map(graph.degree))
     hist.print("Node degree")
     graph
   }
-  
+
   def asMarkerSet(key: String) = MarkerSet.unpack(space, key).fixMarkers.canonical
-    
+
   def main(args: Array[String]) {
-  
+
     args(0) match {
       case "build" =>
         val k = args(1).toInt //e.g. 31
         val numMarkers = args(2).toInt //e.g. 4
         val dbfile = args(3)
 
-        new SeqPrintBuckets(space, k, numMarkers, dbfile).handle(
+        val spb = new SeqPrintBuckets(space, k, numMarkers, dbfile)
+        spb.handle(
           FlatQ.stream(Console.in.lines().iterator))
+
+        println("")
+        var hist = spb.db.bucketSizeHistogram()
+        hist.print("Bucket size (sequences)")
+        hist = spb.db.kmerHistogram
+        hist.print("Bucket size (kmers)")
       case "graph" =>
         Stats.begin()
         val kms = new KmerSpace()
@@ -109,27 +116,27 @@ object SeqPrintBuckets {
         val numMarkers = args(2).toInt //e.g. 4
         val dbfile = args(3)
         val buckets = new SeqPrintBuckets(space, k, numMarkers, dbfile)
-        val graph = makeGraph(kms, buckets)   
+        val graph = makeGraph(kms, buckets)
         Stats.end("Construct graph")
-        
+
         Stats.begin()
         val mg = new MacroGraph(graph)
         val parts = mg.partition(1000)
         Stats.end("Partition graph")
-        
+
         val hist = new Histogram(parts.map(_.size), 20)
         hist.print("Macro partition size")
         val numKmers = parts.map(p => buckets.db.getBulk(p.map(_.packedString)).map(_._2.size).sum)
-        new Histogram(numKmers, 20).print("Macro partition expanded size")    
-        
+        new Histogram(numKmers, 20).print("Macro partition expanded size")
+
         Stats.begin()
         val colGraph = CollapsedGraph.construct(parts, graph)
         Stats.end("Collapse graph")
-        
-        GraphViz.writeUndirected[CollapsedGraph.G[MarkerSet]](colGraph, "out.dot", 
+
+        GraphViz.writeUndirected[CollapsedGraph.G[MarkerSet]](colGraph, "out.dot",
             ms => ms.nodes.size + ":" + ms.nodes.head.packedString)
     }
 
   }
- 
+
 }
--- src/main/scala/dbpart/ubucket/UBucketDB.scala
+++ src/main/scala/dbpart/ubucket/UBucketDB.scala
@@ -11,32 +11,32 @@ import scala.collection.mutable.ArrayBuffer
  * k-mers with identical sequence data are stored together.
  */
 class BucketDB(location: String, options: String, separator: String = "\n") {
-  
+
   def addHook() {
     Runtime.getRuntime.addShutdownHook(new Thread {
       override def run() {
-        println(s"Close $location")        
-        db.close()        
+        println(s"Close $location")
+        db.close()
       }
-    })   
+    })
   }
-  
+
   val db = new DB()
   if (!db.open(s"$location$options", DB.OWRITER | DB.OCREATE)) {
     throw new Exception("Unable to open db")
   }
   println(s"$location open")
-  
+
   addHook()
-  
-  
+
+
   /**
    * The default bucket implementation simply adds to a list.
    * Overriding methods can be more sophisticated, e.g. perform sorting or deduplication
    */
-  def setInsertSingle(oldSet: String, value: String): Option[String] = 
-    Some(s"$oldSet$separator$value")  
-  
+  def setInsertSingle(oldSet: String, value: String): Option[String] =
+    Some(s"$oldSet$separator$value")
+
   /**
    * Insert a number of values into the set, returning the updated set if an update is necessary.
    * Returns None if no update is needed.
@@ -45,13 +45,13 @@ class BucketDB(location: String, options: String, separator: String = "\n") {
     val newVals = values.mkString(separator)
     setInsertSingle(oldSet, newVals)
   }
-  
+
   def newSet(values: Iterable[String]) =
     values.mkString(separator)
-  
-  def unpackSet(set: String): Iterable[String] = set.split(separator, -1)    
-    
-  def addSingle(key: String, record: String) {    
+
+  def unpackSet(set: String): Iterable[String] = set.split(separator, -1)
+
+  def addSingle(key: String, record: String) {
     val v = Option(db.get(key))
     v match {
       case Some(value) =>
@@ -62,37 +62,37 @@ class BucketDB(location: String, options: String, separator: String = "\n") {
           case Some(nv) => db.set(key, nv)
           case None =>
         }
-      case None => 
+      case None =>
         db.set(key, record)
     }
-    
+
     db.cursor()
   }
-  
+
   import scala.collection.{Map => CMap}
   import scala.collection.mutable.{Map => MMap}
-  
+
   def merge(oldVals: MMap[String, String], from: CMap[String, Iterable[String]]) = {
-    var r = MMap[String, String]()    
+    var r = MMap[String, String]()
     for ((k, vs) <- from) {
       oldVals.get(k) match {
         case Some(existingSet) =>
           setInsertBulk(existingSet, vs) match {
-            case Some(ins) => 
-              r += k -> ins              
+            case Some(ins) =>
+              r += k -> ins
             case None =>
-          }          
+          }
         case None =>
-          r += k -> newSet(vs)          
+          r += k -> newSet(vs)
       }
-    }    
+    }
     r
   }
-  
-  def addBulk(data: Iterable[(String, String)]) {    
+
+  def addBulk(data: Iterable[(String, String)]) {
     val insert = data.groupBy(_._1).mapValues(vs => vs.map(_._2))
 //    Distribution.printStats("Insertion buckets", insert.map(_._2.size))
-    
+
     for (insertGr <- insert.grouped(1000).toSeq.par) {
       val existing = db.get_bulk(seqAsJavaList(insertGr.keys.toSeq), false)
       //    val existing = MMap[String, String]()
@@ -100,16 +100,16 @@ class BucketDB(location: String, options: String, separator: String = "\n") {
       db.set_bulk(merged.asJava, false)
     }
   }
-  
+
   def getBulk(keys: Iterable[String]): CMap[String, Iterable[String]] = {
-    db.get_bulk(seqAsJavaList(keys.toSeq), false).asScala.mapValues(unpackSet)    
+    db.get_bulk(seqAsJavaList(keys.toSeq), false).asScala.mapValues(unpackSet)
   }
-  
+
   def buckets: Iterator[(String, Iterable[String])] = {
 
     new Iterator[(String, Iterable[String])] {
       val cur = db.cursor()
-      cur.jump()      
+      cur.jump()
       var continue = cur.step()
       var nextVal: Array[String] = cur.get_str(true)
 
@@ -131,17 +131,16 @@ class BucketDB(location: String, options: String, separator: String = "\n") {
   }
 
   def bucketSizeStats() = {
-    val r = new Distribution    
+    val r = new Distribution
     for ((b, vs) <- buckets) {
       r.observe(vs.size)
     }
     r
   }
-  
+
   def bucketSizeHistogram() = {
     val ss = buckets.map(_._2.size)
-    val h = new Histogram(ss.toSeq)
-    h
+    new Histogram(ss.toSeq)
   }
 
 }
@@ -149,12 +148,12 @@ class BucketDB(location: String, options: String, separator: String = "\n") {
 /**
  * BucketDB implementation that deduplicates entries.
  */
-class UBucketDB(location: String, options: String, separator: String = "\n") 
+class UBucketDB(location: String, options: String, separator: String = "\n")
 extends BucketDB(location, options, separator) {
 
-  override def setInsertSingle(oldSet: String, value: String): Option[String] = 
-    setInsertBulk(oldSet, Seq(value))  
-  
+  override def setInsertSingle(oldSet: String, value: String): Option[String] =
+    setInsertBulk(oldSet, Seq(value))
+
   import scala.collection.mutable.{Set => MSet}
   override def setInsertBulk(oldSet: String, values: Iterable[String]): Option[String] = {
     val old = MSet() ++ unpackSet(oldSet)
@@ -166,27 +165,30 @@ extends BucketDB(location, options, separator) {
       Some(newSet.mkString(separator))
     }
   }
-  
-  override def newSet(values: Iterable[String]) =    
+
+  override def newSet(values: Iterable[String]) =
     values.toSeq.distinct.mkString(separator)
-  
+
 }
 
 /**
  * BucketDB that merges k-mers into contiguous paths.
+ * Buckets will contain lists of paths, not KMers.
+ * To obtain individual KMers, methods such as kmerBuckets and kmerHistogram
+ * can be used.
  */
-class PathBucketDB(location: String, options: String, k: Int, separator: String = "\n") 
+class PathBucketDB(location: String, options: String, k: Int, separator: String = "\n")
 extends BucketDB(location, options, separator) {
-  
-  override def setInsertSingle(oldSet: String, value: String): Option[String] = 
+
+  override def setInsertSingle(oldSet: String, value: String): Option[String] =
     setInsertBulk(oldSet, Seq(value))
- 
+
   override def setInsertBulk(oldSet: String, values: Iterable[String]): Option[String] = {
     val old = unpackSet(oldSet)
     var r: ArrayBuffer[String] = new ArrayBuffer(values.size + old.size)
     r ++= old
-    var updated = false    
-    for (v <- values; if !seqExists(v, r)) {      
+    var updated = false
+    for (v <- values; if !seqExists(v, r)) {
       insertSequence(v, r)
       updated = true
     }
@@ -196,7 +198,7 @@ extends BucketDB(location, options, separator) {
       None
     }
   }
-  
+
   override def newSet(values: Iterable[String]) = {
     setInsertBulk(values.head, values.tail).getOrElse(values.head)
   }
@@ -211,12 +213,12 @@ extends BucketDB(location, options, separator) {
     }
     false
   }
-   
+
   /**
    * Insert a new sequence into a set of pre-existing sequences, by merging if possible.
    */
-  final def insertSequence(data: String, into: ArrayBuffer[String]) {    
-    
+  final def insertSequence(data: String, into: ArrayBuffer[String]) {
+
     val suffix = data.substring(1)
     val prefix = data.substring(0, k - 1)
     var i = 0
@@ -229,11 +231,31 @@ extends BucketDB(location, options, separator) {
       if (existingSeq.endsWith(prefix)) {
         into(i) = (existingSeq + data.charAt(data.length() - 1))
         return
-      } 
+      }
       i += 1
-    }        
-    into += data    
+    }
+    into += data
   }
+
+  def kmerBuckets = {
+    buckets.map((kv) => (kv._1, kv._2.flatMap(read => {
+      read.sliding(k)
+    })))
+  }
+
+  def kmerStats = {
+    val r = new Distribution
+    for ((b, vs) <- kmerBuckets) {
+      r.observe(vs.size)
+    }
+    r
+  }
+
+  def kmerHistogram = {
+    val ss = kmerBuckets.map(_._2.size)
+    new Histogram(ss.toSeq)
+  }
+
 }
 
 object UBucketDB {
@@ -243,14 +265,14 @@ object UBucketDB {
   val c8g = 8 * c1g
   val c20g = 20L * c1g
 
-  //40M buckets 
+  //40M buckets
   //256b byte alignment
   //8G mmap
   //#comp=zlib
   val options = s"#msiz=$c20g#bnum=40000000#apow=8"
-  
+
   def main(args: Array[String]) {
-   
+
     args(0) match {
       case "add" =>
         val file = args(1)
@@ -264,21 +286,21 @@ object UBucketDB {
       case "graph" =>
         val file = args(1)
         val db = new UBucketDB(file, options)
-        
+
       case _ => throw new Exception("Unxpected command")
-    }    
+    }
   }
-  
-  
+
+
   val k = 31
   def kmers(read: String) = read.sliding(k)
   //2^20 = 1M possible keys
   def key(kmer: String) = kmer.substring(0, 10)
-    
+
   def addFromStream(db: UBucketDB, flatq: Iterator[(String, String)]) {
     for (group <- flatq.grouped(10000);
         kvs = group.map(x => (key(x._1), x._1))) {
-      db.addBulk(kvs)      
+      db.addBulk(kvs)
     }
   }
 }
