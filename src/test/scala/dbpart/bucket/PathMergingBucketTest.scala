df1ac2b1f41f6a681ec021a044553fff9ed41cb0
Johan Nystrom <johan@monomorphic.org>
2019-06-30 14:15:03 +0900

    Update PathMergingBucketTest to new API

--- src/test/scala/dbpart/bucket/PathMergingBucketTest.scala
+++ src/test/scala/dbpart/bucket/PathMergingBucketTest.scala
@@ -6,38 +6,31 @@ import org.scalatest.FunSuite
 
 class PathMergingBucketTest extends FunSuite with Matchers {
   import Testing._
-  
-  test("basic") {
-    //Construct PathMergingBucket
+
+  test("sharedOverlaps") {
+
     //Check that in- and out-openings are correct
     val k = 5
-
     val ss = Array("ACTGGG", "TTGTTA")
     var post = Array("CCGAT", "GTTAA")
     var prior = Array[String]()
-    var bucket = PathMergingBucket(ss, k).withPost(post).
-      withPrior(prior)
-    bucket.outOpenings should equal (Seq("GTTA"))
-    bucket.inOpenings should equal (Seq())
-    
+    PathMergingBucket.sharedOverlaps(prior, ss, k) should equal (Seq())
+    PathMergingBucket.sharedOverlaps(ss, post, k) should equal (Seq("GTTA"))
+
     prior = Array("CCGAG", "AACTGAA")
-    bucket = PathMergingBucket(ss, k).withPost(post).
-      withPrior(prior)
-    bucket.inOpenings should equal (Seq("ACTG"))
-    
-    
+    PathMergingBucket.sharedOverlaps(prior, ss, k) should equal(Seq("ACTG"))
+
 //    val post = Array("CCGAT", "GTTAA")
   }
-  
+
   test("containedUnitigs") {
     // Check that unitigs entirely contained in buckets (not touching boundary)
     // are identified
   }
-  
-  
+
   test("seizeUnitigs") {
     //Check that unitigs contained entirely in the bucket
     //can be obtained, and that they do not remain in the bucket
     //afterward
-  }  
+  }
 }
