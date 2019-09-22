aed3bde0cbe34f9c09e9b7c68b47a5f44a7d3a4c
Johan Nystrom <johan@monomorphic.org>
2019-09-22 21:59:00 +0900

    Speed up Util.partitionByKeys

--- src/main/scala/dbpart/bucket/Util.scala
+++ src/main/scala/dbpart/bucket/Util.scala
@@ -6,34 +6,46 @@ import scala.annotation.tailrec
 
 object Util {
 
+  type WithKeys[A, K] = (List[A], Iterable[K])
+
   /**
    * Given a collection and a function that produces multiple keys from each item,
    * produces a partitioning such that items are in the same partition iff they
    * share at least one key.
+   * Items are expected to be distinct.
    */
   def partitionByKeys[A, K](items: List[A], keys: A => Iterable[K]): List[List[A]] = {
-    var r = List[List[A]]()
-    for {
-      i <- items
-      ks = keys(i).to[MSet]
-    } {
-      r = unify(List(i), ks, r, keys)
-    }
-    r
+    val withKeys = items.map(i => (i, keys(i)))
+
+    withKeys.foldLeft(List[WithKeys[A, K]]())((acc, i) => {
+      val ks = i._2.to[MSet]
+      unify(List(i._1), ks, acc, keys)
+    }).map(_._1)
   }
 
+  /**
+   * Iterate through clusters, unifying them
+   * @param from list to unify with as many clusters as possible
+   * @param fromKeys keys of from
+   * @param clusters iterate through this list of existing clusters
+   * @param keys key construction function
+   * @param acc result accumulator
+   * @tparam A
+   * @tparam K
+   * @return
+   */
   @tailrec
   def unify[A, K](from: List[A], fromKeys: CSet[K],
-      lists: List[List[A]], keys: A => Iterable[K], acc: List[List[A]] = Nil): List[List[A]] = {
-    lists match {
-      case l :: ls =>
-        val ks = l.iterator.flatMap(keys)
-        if (ks.filter(fromKeys.contains).nonEmpty) {
-          unify(l ::: from, fromKeys, ls, keys, acc)
+                  clusters: List[WithKeys[A, K]],
+                  keys: A => Iterable[K], acc: List[WithKeys[A, K]] = Nil): List[WithKeys[A, K]] = {
+    clusters match {
+      case (l, ks) :: ls =>
+        if (ks.iterator.filter(fromKeys.contains).nonEmpty) {
+          unify(l ::: from, fromKeys ++ ks, ls, keys, acc)
         } else {
-          unify(from, fromKeys, ls, keys, l :: acc)
+          unify(from, fromKeys, ls, keys, (l, ks) :: acc)
         }
-      case _ => from :: acc
+      case _ => (from, fromKeys.toSeq):: acc
     }
   }
 }
